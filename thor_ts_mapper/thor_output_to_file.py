5fce1026211c189e4b38a386251697d5e7e64274
icicala <icicala.ubb@gmail.com>
2025-04-17 14:05:00 +0200

    fix: the creation on error an empty file

--- thor_ts_mapper/thor_output_to_file.py
+++ thor_ts_mapper/thor_output_to_file.py
@@ -3,6 +3,8 @@ import json
 from typing import Dict, Any, Iterator
 from thor_ts_mapper.logger_config import LoggerConfig
 from alive_progress import alive_bar
+from thor_ts_mapper import constants
+from thor_ts_mapper.exceptions import OutputError
 
 
 logger = LoggerConfig.get_logger(__name__)
@@ -11,15 +13,14 @@ logger = LoggerConfig.get_logger(__name__)
 class THOROutputToFile:
     def __init__(self, output_file: str):
         self.output_file = output_file
-        self._prepare_output_dir()
         self.mode = 'a' if os.path.exists(self.output_file) else 'w'
 
-    def _validate_output_file(self)  -> None:
+    def _validate_file_extension(self)  -> None:
         try:
-            if not self.output_file.lower().endswith('.jsonl'):
-                original = self.output_file
-                self.output_file = os.path.splitext(self.output_file)[0] + '.jsonl'
-                logger.info(f"Changed output file from `{original}` to `{self.output_file}` to ensure JSONL format")
+            file_name, extension = os.path.splitext(self.output_file)
+            if extension.lower() != constants.OUTPUT_FILE_EXTENSION:
+                self.output_file = file_name + constants.OUTPUT_FILE_EXTENSION
+                logger.info(f"Changed output file from `{extension}` to `{self.output_file}` to ensure JSONL format")
         except Exception as e:
             logger.error("Error validating or modifying the output file: %s", e)
 
@@ -31,17 +32,27 @@ class THOROutputToFile:
                 os.makedirs(output_dir, exist_ok=True)
             except Exception as e:
                 logger.error(f"Failed to create output directory `{output_dir}`: {e}")
+                raise OutputError(f"Cannot create output directory: {e}")
 
 
     def write_to_file(self, events: Iterator[Dict[str, Any]]) -> None:
-        self._validate_output_file()
-        self._prepare_output_dir()
-        with alive_bar(spinner='dots', title=f"Writing to {self.output_file}") as bar:
-            with open(self.output_file, self.mode, encoding='utf-8') as file:
-                for event in events:
-                    try:
+        try:
+            self._validate_file_extension()
+            self._prepare_output_dir()
+            with alive_bar(spinner='dots', title=f"Writing to {self.output_file}") as bar:
+                with open(self.output_file, self.mode, encoding=constants.DEFAULT_ENCODING) as file:
+                    for event in events:
                         file.write(json.dumps(event) + "\n")
                         bar()
-                    except Exception as e:
-                        logger.error("Error writing event: %s", e)
-            logger.debug("Successfully written events to %s", self.output_file)
+                logger.debug(f"Successfully written events to {self.output_file}")
+        except Exception as e:
+            if self.output_file and os.path.exists(self.output_file):
+                try:
+                    os.remove(self.output_file)
+                except OSError as e:
+                    error_msg = f"Failed to remove output file: {e}"
+                    logger.error(error_msg)
+                    raise OutputError(error_msg)
+            error_msg = f"Error writing to file: {e}"
+            logger.error(error_msg)
+            raise OutputError(error_msg)
