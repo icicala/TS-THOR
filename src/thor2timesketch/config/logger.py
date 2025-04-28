c782ca84ed3198d7ff32134905797daefe166d80
icicala <icicala.ubb@gmail.com>
2025-04-28 12:07:58 +0200

    fix: back to default values

--- src/thor2timesketch/config/logger.py
+++ src/thor2timesketch/config/logger.py
@@ -2,26 +2,6 @@ import logging
 from rich.console import Console
 from rich.logging import RichHandler
 
-
-class ColorFormatter(logging.Formatter):
-    def format(self, record: logging.LogRecord) -> str:
-        time_str = self.formatTime(record, self.datefmt)
-        if record.levelno == logging.DEBUG:
-            level_color = "[blue]"
-        elif record.levelno == logging.INFO:
-            level_color = "[green]"
-        elif record.levelno == logging.WARNING:
-            level_color = "[yellow]"
-        elif record.levelno == logging.ERROR:
-            level_color = "[red]"
-        else:
-            level_color = "[white]"
-
-        level_str = f"{level_color}{record.levelname}[/]"
-        message_str = f"[white]{record.getMessage()}[/white]"
-        return f"[white]{time_str}[/white] {level_str} {message_str}"
-
-
 class LoggerConfig:
     console = Console()
 
@@ -32,13 +12,10 @@ class LoggerConfig:
             rich_tracebacks=True,
             show_path=False,
             markup=True,
-            show_time=False,
-            show_level=False,
+            show_time=True,
+            show_level=True,
             enable_link_path=False
         )
-
-        formatter = ColorFormatter("%(message)s", datefmt="[%Y-%m-%d %H:%M:%S]")
-        handler.setFormatter(formatter)
 
         logging.basicConfig(
             level=level,
